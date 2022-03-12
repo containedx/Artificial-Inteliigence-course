1feed8a7095fcaae6c718742921287317fe1517b
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-12 17:33:20 +0100

    Going to destinations by Navmesh - sooo cool

---
+++ AI Project-Unity2019/Assets/Waypoints&Navmesh/FollowPath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public GameObject WaypointsManager;
+    public GameObject[] waypoints;
+    UnityEngine.AI.NavMeshAgent NavMeshAgent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //waypoints = WaypointsManager.GetComponent<WPManager>().waypoints;
+        NavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+    }
+
+    public void GoToOilField(){
+        //Oilfield == WP005 
+        NavMeshAgent.SetDestination(waypoints[14].transform.position);
+    }
+
+    public void GoToRocks(){
+        //rocks == WP013
+        NavMeshAgent.SetDestination(waypoints[17].transform.position);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+    }
+}
