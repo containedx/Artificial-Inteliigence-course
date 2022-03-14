03ed2b2404624d9034aaf02ad7fba733dcd8a396
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-14 11:17:54 +0100

    zombie chasing player

---
+++ AI Project-Unity2019/Assets/Zombies/ChasePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+    GameObject target; //player
+    NavMeshAgent agent;
+
+
+    void Start(){
+        target = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        agent.SetDestination(target.transform.position);
+    }
+}
