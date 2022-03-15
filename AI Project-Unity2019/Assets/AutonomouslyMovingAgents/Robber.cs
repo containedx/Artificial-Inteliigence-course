cc9bfe6652476ecfd521c35f276bf1cca1e0adb0
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-15 13:43:27 +0100

    Seek

---
+++ AI Project-Unity2019/Assets/AutonomouslyMovingAgents/Robber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Robber : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Seek(target.transform.position);
+    }
+
+
+    void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+}
