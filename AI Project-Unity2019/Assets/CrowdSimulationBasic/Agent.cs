ef8c61bf485f174011a18fa823be1be43dc4b1ec
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-16 11:53:28 +0100

    crowd simulation

---
+++ AI Project-Unity2019/Assets/CrowdSimulationBasic/Agent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public GameObject goal;
+    public NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent.SetDestination(goal.transform.position);
+    }
+
+}
