0a09099d3bd17577ab283c998a0e82a46808ffac
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-16 12:26:31 +0100

    crowd simulation on street starter

--- AI Project-Unity2019/Assets/StreetScene 3/AIControl.cs
+++ AI Project-Unity2019/Assets/StreetScene 3/AIControl.cs
@@ -8,16 +8,29 @@ public class AIControl : MonoBehaviour {
 	GameObject[] goalLocations;
 	UnityEngine.AI.NavMeshAgent agent;
 
+	Animator animator;
+
 
 	// Use this for initialization
 	void Start () {
 		goalLocations = GameObject.FindGameObjectsWithTag("goal");
 		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		animator = GetComponent<Animator>();
+
+		PickRandomGoal();
+		//trigger walking animation
+		animator.SetTrigger("isWalking");
+	}
+
+	void Update(){
+		// if goal reached, pick another goal
+		if (agent.remainingDistance < 1){
+			PickRandomGoal();
+		}
+	}
+
+	void PickRandomGoal(){
 		agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
