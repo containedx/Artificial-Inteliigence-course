8b7470573eaee0d972a82165a11c55deadeee4be
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-17 15:44:28 +0100

    obstacle and second flock

--- AI Project-Unity2019/Assets/Fishes/FishAgent.cs
+++ AI Project-Unity2019/Assets/Fishes/FishAgent.cs
@@ -2,89 +2,110 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishAgent : MonoBehaviour
-{
-    public FlockManager manager;
-    float speed;
+public class FishAgent : MonoBehaviour {
 
-    bool turning = false;
+	public FlockManager myManager;
+	public float speed;
+	bool turning = false;
 
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(myManager.minSpeed,
+								myManager.maxSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
 
-    // Start is called before the first frame update
-    void Start(){
-        speed = Random.Range(manager.minSpeed, manager.maxSpeed);
-    }
+		//determine the bounding box of the manager cube
+		Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits*2);
+		
+		//if fish is outside the bounds of the cube or about to hit something 
+		//then start turning around
+		RaycastHit hit = new RaycastHit();
+		Vector3 direction = Vector3.zero;
 
-    // Update is called once per frame
-    void Update(){
-        KeepInBounds();
+		if(!b.Contains(transform.position) )
+		{
+			turning = true;
+			direction = myManager.transform.position - transform.position;
+		} 
+		else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+		{
+			turning = true;
+			direction = Vector3.Reflect(this.transform.forward,hit.normal);
+		}
+		else
+			turning = false;
 
-        if(!turning){
-        if(Random.Range(0,100) < 20)
-            ApplyRules();
-        if(Random.Range(0,100) < 10)
-            speed = Random.Range(manager.minSpeed, manager.maxSpeed);
-        }
+		if(turning)
+		{
+			//turn towards the centre of the manager cube
+			
+			transform.rotation = Quaternion.Slerp(transform.rotation,
+					                                  Quaternion.LookRotation(direction), 
+					                                  myManager.rotationSpeed * Time.deltaTime);
+		}
+		else
+		{  
 
-        
-        MoveForward();
-    }
+			if(Random.Range(0,100) < 10)
+				speed = Random.Range(myManager.minSpeed,
+								myManager.maxSpeed);
+			if(Random.Range(0,100) < 20)
+				ApplyRules();
+		}
+		
+		MoveForward();
+	}
 
-    void KeepInBounds(){
-        Bounds b = new Bounds(manager.transform.position, manager.swimLimits*2);
+	void ApplyRules()
+	{
+		GameObject[] gos;
+		gos = myManager.allFish;
+		
+		Vector3 vcentre = Vector3.zero;
+		Vector3 vavoid = Vector3.zero;
+		float gSpeed = 0.01f;
+		float nDistance;
+		int groupSize = 0;
 
-        if(!b.Contains(transform.position)){
-            turning = true;
-        }
-        else{
-            turning = false;
-        }
-
-        if(turning){
-            Vector3 direction = manager.transform.position - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), manager.rotationSpeed*Time.deltaTime);
-        }
-    }
-
-    void ApplyRules(){
-        GameObject[] neighbours = manager.fishes;
-
-        Vector3 vcentre = Vector3.zero;
-        Vector3 vavoid = Vector3.zero;
-
-        float gSpeed = 0.01f;
-        float nDistance;
-        int groupSize = 0;
-
-        foreach (var n in neighbours){
-            if(n != this.gameObject){
-                nDistance = Vector3.Distance(n.transform.position, transform.position);
-                if(nDistance <= manager.neighbourDistance){
-                    vcentre = vcentre + n.transform.position;
-                    groupSize++;
-
-                    if(nDistance < 1.0f){
-                        vavoid = vavoid + (transform.position - n.transform.position);
-                    }
-
-                    FishAgent anotherAgent = n.GetComponent<FishAgent>();
-                    gSpeed = gSpeed + anotherAgent.speed;
-                }
-            }
-        }
-
-        if(groupSize > 0){
-            vcentre = vcentre/groupSize + manager.goalPosition - transform.position;
-            speed = gSpeed/groupSize;
-
-            Vector3 direction = (vcentre + vavoid) - transform.position;
-            if(direction != Vector3.zero){
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), manager.rotationSpeed * Time.deltaTime);
-            }
-        }
-    }
+		foreach (GameObject go in gos) 
+		{
+			if(go != this.gameObject)
+			{
+				nDistance = Vector3.Distance(go.transform.position,this.transform.position);
+				if(nDistance <= myManager.neighbourDistance)
+				{
+					vcentre += go.transform.position;	
+					groupSize++;	
+					
+					if(nDistance < 1.0f)		
+					{
+						vavoid = vavoid + (this.transform.position - go.transform.position);
+					}
+					
+					FishAgent anotherFlock = go.GetComponent<FishAgent>();
+					gSpeed = gSpeed + anotherFlock.speed;
+				}
+			}
+		} 
+		
+		if(groupSize > 0)
+		{
+			vcentre = vcentre/groupSize + (myManager.goalPos - this.transform.position);
+			speed = gSpeed/groupSize;
+			
+			Vector3 direction = (vcentre + vavoid) - transform.position;
+			if(direction != Vector3.zero)
+				transform.rotation = Quaternion.Slerp(transform.rotation,
+					                                  Quaternion.LookRotation(direction), 
+					                                  myManager.rotationSpeed * Time.deltaTime);
+		
+		}
+	}
 
     void MoveForward(){
-        transform.Translate(0,0,Time.deltaTime * speed);
+        transform.Translate(0, 0, Time.deltaTime * speed);
     }
 }
--- AI Project-Unity2019/Assets/Fishes/FlockManager.cs
+++ AI Project-Unity2019/Assets/Fishes/FlockManager.cs
@@ -2,47 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlockManager : MonoBehaviour
-{
-    public GameObject fishPrefab;
-    public int numberOfFishes = 100;
-    public GameObject[] fishes;
-    public Vector3 swimLimits = new Vector3(5,5,5);
-    public Vector3 goalPosition;
+public class FlockManager : MonoBehaviour {
 
-    [Header("Fish Settings")]
-    [Range(0.0f, 5.0f)]
-    public float minSpeed;
-    [Range(0.0f, 5.0f)]
-    public float maxSpeed;
-    [Range(1.0f, 10.0f)]
-    public float neighbourDistance;
-    [Range(0.0f, 5.0f)]
-    public float rotationSpeed;
+	public GameObject fishPrefab;
+	public int numFish = 20;
+	public GameObject[] allFish;
+	public Vector3 swimLimits = new Vector3(5,5,5);
+	public Vector3 goalPos;
 
+	[Header("Fish Settings")]
+	[Range(0.0f, 5.0f)]
+	public float minSpeed;
+	[Range(0.0f, 5.0f)]
+	public float maxSpeed;
+	[Range(1.0f, 10.0f)]
+	public float neighbourDistance;
+	[Range(0.0f, 5.0f)]
+	public float rotationSpeed;
 
-
-    void Start(){
-        fishes = new GameObject[numberOfFishes];
-
-        for (int i = 0; i < numberOfFishes; i++)
-        {
-            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), 
-                                                           Random.Range(-swimLimits.y, swimLimits.y), 
-                                                           Random.Range(-swimLimits.z, swimLimits.z) );
-            fishes[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);  
-            fishes[i].GetComponent<FishAgent>().manager = this; 
-        }
-    }
-
-    void Update(){
-        if(Random.Range(0,100)<10) //10% chance of changing, to not change it every update
-        {
-            goalPosition = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), 
-                                                           Random.Range(-swimLimits.y, swimLimits.y), 
-                                                           Random.Range(-swimLimits.z, swimLimits.z) );
-        }
-
-    }
-
+	// Use this for initialization
+	void Start () {
+		allFish = new GameObject[numFish];
+		for(int i = 0; i < numFish; i++)
+		{
+			Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x,swimLimits.x),
+				                      							Random.Range(-swimLimits.y,swimLimits.y),
+				                      							Random.Range(-swimLimits.z,swimLimits.z));
+			allFish[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
+			allFish[i].GetComponent<FishAgent>().myManager = this;
+		}
+		goalPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0,100)<10)
+			goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x,swimLimits.x),
+				                      					Random.Range(-swimLimits.y,swimLimits.y),
+				                      					Random.Range(-swimLimits.z,swimLimits.z));
+	}
 }
