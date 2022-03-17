41a4e6742f14ae70dec06e2637f7136487a9c5ab
Kinga <kinga.zawarty@pluckybytes.com>
2022-03-17 15:07:35 +0100

    basic fish flock

---
+++ AI Project-Unity2019/Assets/Fishes/FishAgent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAgent : MonoBehaviour
+{
+    public FlockManager manager;
+    float speed;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        speed = Random.Range(manager.minSpeed, manager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        ApplyRules();
+        MoveForward();
+    }
+
+    void ApplyRules(){
+        GameObject[] neighbours;
+        neighbours = manager.fishes;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (var n in neighbours){
+            if(n != this.gameObject){
+                nDistance = Vector3.Distance(n.transform.position, transform.position);
+                if(nDistance <= manager.neighbourDistance){
+                    vcentre = vcentre + n.transform.position;
+                    groupSize++;
+
+                    if(nDistance < 1.0f){
+                        vavoid = vavoid + (transform.position - n.transform.position);
+                    }
+
+                    FishAgent anotherAgent = n.GetComponent<FishAgent>();
+                    gSpeed = gSpeed + anotherAgent.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0){
+            vcentre = vcentre/groupSize;
+            speed = gSpeed/groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if(direction != Vector3.zero){
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), manager.rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    void MoveForward(){
+        transform.Translate(0,0,Time.deltaTime * speed);
+    }
+}
---
+++ AI Project-Unity2019/Assets/Fishes/FlockManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject fishPrefab;
+    public int numberOfFishes = 100;
+    public GameObject[] fishes;
+    public Vector3 swimLimits = new Vector3(5,5,5);
+
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+
+
+
+    void Start(){
+        fishes = new GameObject[numberOfFishes];
+
+        for (int i = 0; i < numberOfFishes; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), 
+                                                           Random.Range(-swimLimits.y, swimLimits.y), 
+                                                           Random.Range(-swimLimits.z, swimLimits.z) );
+            fishes[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);  
+            fishes[i].GetComponent<FishAgent>().manager = this; 
+        }
+    }
+
+}
